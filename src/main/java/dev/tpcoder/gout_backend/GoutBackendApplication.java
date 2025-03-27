aa4680aeda887709213e0ebe65254cc57f5f80fd
KanathipP <miniexor@gmail.com>
2025-03-27 18:39:52 +0700

    initialize project

---
+++ src/main/java/dev/tpcoder/gout_backend/GoutBackendApplication.java
@@ -0,0 +1,15 @@
+package dev.tpcoder.gout_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GoutBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GoutBackendApplication.class, args);
+	}
+
+	
+
+}
