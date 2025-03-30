f43b329e35274e0d173ae83ae152cd298b0c8e76
KanathipP <kanathipz.p@gmail.com>
2025-03-30 22:02:12 +0700

    test

--- src/main/java/dev/tpcoder/gout_backend/GoutBackendApplication.java
+++ src/main/java/dev/tpcoder/gout_backend/GoutBackendApplication.java
@@ -8,8 +8,8 @@ public class GoutBackendApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(GoutBackendApplication.class, args);
+		System.out.println("hehe");
 	}
-
 	
 
 }
